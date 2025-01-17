77d570a58d7b155036b530b0a24f8dca07be5aa6
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-16 22:27:48 -0300

    Criação da classe AbstractEntity com os atributos comuns entre todas as classes do projeto

---
+++ stock/src/main/java/com/stockapi/stock/entity/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.stockapi.stock.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.util.QTypeContributor;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cl_id", nullable = false, unique = true)
+    private Long id;
+    @Getter @Setter
+    @Column(name = "cl_ativo",nullable = false)
+    private boolean ativo;
+    @Getter @Setter
+    @Column(name = "cl_registro", nullable = false)
+    private LocalDateTime registro;
+    @Getter @Setter
+    @Column(name = "cl_atualizar")
+    private LocalDateTime atualizar;
+    @PrePersist
+    void prePersist(){
+        this.registro = LocalDateTime.now();
+        this.ativo = true;
+    }
+    @PreUpdate
+    void preUpdate(){
+        this.atualizar = LocalDateTime.now();
+    }
+}
