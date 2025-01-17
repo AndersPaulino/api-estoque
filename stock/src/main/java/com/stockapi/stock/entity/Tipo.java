03e5a1c20af48430b40bb8027a286191a304a199
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-16 23:42:14 -0300

    Criação de metodos Getters e Setters, devido as Annotations @Getter e @Setter do Lombok não estarem sendo interpretadas e executadas corretamente

--- stock/src/main/java/com/stockapi/stock/entity/Tipo.java
+++ stock/src/main/java/com/stockapi/stock/entity/Tipo.java
@@ -3,13 +3,19 @@ package com.stockapi.stock.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "tb_tipo", schema = "public")
 public class Tipo extends AbstractEntity{
-    @Getter @Setter
+
     @Column(name = "cl_nome_tipo")
     private String nomeTipo;
+
+    public String getNomeTipo() {
+        return nomeTipo;
+    }
+
+    public void setNomeTipo(String nomeTipo) {
+        this.nomeTipo = nomeTipo;
+    }
 }
