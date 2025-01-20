90b886e280d365256924a259084b5927303afb9e
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-20 11:30:30 -0300

    Correções na classe Produto

--- stock/src/main/java/com/stockapi/stock/controller/ProdutoController.java
+++ stock/src/main/java/com/stockapi/stock/controller/ProdutoController.java
@@ -2,11 +2,13 @@ package com.stockapi.stock.controller;
 
 import com.stockapi.stock.dto.ProdutoDTO;
 import com.stockapi.stock.entity.Produto;
+import com.stockapi.stock.entity.Tipo;
 import com.stockapi.stock.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.List;
