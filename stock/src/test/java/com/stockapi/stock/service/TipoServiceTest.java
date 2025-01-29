72c5071a422ffaf22617d9988f741bf7a71fe494
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-28 22:47:31 -0300

    Criação do test findByIdNotFound da Classe TipoServiceTest

--- stock/src/test/java/com/stockapi/stock/service/TipoServiceTest.java
+++ stock/src/test/java/com/stockapi/stock/service/TipoServiceTest.java
@@ -4,10 +4,16 @@ import com.stockapi.stock.dto.TipoDTO;
 import com.stockapi.stock.entity.Tipo;
 import com.stockapi.stock.repository.TipoRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TipoServiceTest {
@@ -28,5 +34,15 @@ public class TipoServiceTest {
         tipo = new Tipo();
         tipo.setNomeTipo("tipo");
         tipoDTO = new TipoDTO(tipo);
+    }
+
+    @Test
+    void findByIdNotFound(){
+        Long id = 1L;
+        when(tipoRepository.findById(id)).thenReturn(Optional.empty());
+
+        Optional<TipoDTO> result = tipoService.findById(id);
+
+        assertFalse(result.isPresent());
     }
 }
